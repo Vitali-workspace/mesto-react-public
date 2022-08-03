ad818ebd9873959866a919672fda9b5883071154
Umbra <icofundss@gmail.com>
2022-08-03 22:58:20 +0300

    refactor: изменить подключение компонентов

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import App from './components/App';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Header />
-    <Main />
-
-    <Footer />
   </React.StrictMode>
 );
 
