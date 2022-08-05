e547dd0580ec5a6f8e25eb48cbecdb7da4b41f20
Umbra <icofundss@gmail.com>
2022-08-05 22:25:01 +0300

    feat: добавить состояния и эффекты для компонента Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,51 @@
-function Main() {
+
+import React from 'react';
+import photo from '../images/image-prof.png';
+import api from '../utils/Api.js';
+import Card from './Card.js';
+//style={{ backgroundImage: `url(${userAvatar})` }}
+
+function Main(props) {
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+  const [cards, setCards] = React.useState([]);
+
+  console.log(api);
+  api.getInitialCards().then((info) => { console.log(info); })
+
+  React.useState(() => {
+    api.getInitialCards()
+      .then(info => {
+        setUserName(info.name)
+        setUserDescription(info.description)
+        setUserAvatar(info.avatar)
+        console.log(info.name);
+      }).catch(err => console.log(err))
+  }, []);
+
+  React.useState(() => {
+    api.getProfileInfo()
+      .then(info => {
+        setCards(info.cards)
+        console.log(info.cards);
+      }).catch(err => console.log(err))
+  }, []);
+
+
   return (
-    <main class="page">
-      <section class="profile">
-        <div class="profile__section-avatar">
-          <button class="profile__edit-icon"></button>
-          <img class="profile__photo" src="<%=require('./images/image-prof.png')%>" alt="фото профиля" />
+    <main className="page">
+      <section className="profile">
+        <div className="profile__section-avatar">
+          <button className="profile__edit-icon" onClick={props.onEditAvatar}></button>
+          <img className="profile__photo" src={photo} alt="фото профиля" />
         </div>
-        <h1 class="profile__name">Жак-Ив Кусто</h1>
-        <p class="profile__description">Исследователь океана</p>
-        <button class="profile__btn-edit" type="button"></button>
-        <button class="profile__btn-add" type="button"></button>
+        <h1 className="profile__name">{userName}</h1>
+        <p className="profile__description">{userDescription}</p>
+        <button className="profile__btn-edit" type="button" onClick={props.onEditProfile}></button>
+        <button className="profile__btn-add" type="button" onClick={props.onAddPlace}></button>
       </section>
-      <section class="gallery" aria-label="галерея"></section>
+      <section className="gallery" aria-label="галерея"></section>
     </main>
   )
 }
