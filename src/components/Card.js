e09f03d81aa7f4074d72fb0bc1a8f347d4049b65
Umbra <icofundss@gmail.com>
2022-08-06 16:39:16 +0300

    feat: добавить свойства в компонент Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
-function Card() {
+function Card(props) {
+
   return (
-    <template id="tempCard">
-      <article className="gallery__card">
-        <img className="gallery__card-img" src="#" alt="#" />
-        <button className="gallery__btn-trash"></button>
-        <div className="gallery__card-header">
-          <h3 className="gallery__card-name"> </h3>
-          <div className="gallery__favorites-header">
-            <button className="gallery__btn-favorites" type="button"></button>
-            <div className="gallery__counter-favorites">0</div>
-          </div>
+    <article className="gallery__card">
+      <img className="gallery__card-img" src={`${props.card.link}`} alt={`${props.card.name}`} />
+      <button className="gallery__btn-trash"></button>
+      <div className="gallery__card-header">
+        <h3 className="gallery__card-name">{props.card.name}</h3>
+        <div className="gallery__favorites-header">
+          <button className="gallery__btn-favorites" type="button"></button>
+          <div className="gallery__counter-favorites">{props.card.likes.length}</div>
         </div>
-      </article>
-    </template>
+      </div>
+    </article>
   )
 }
 
