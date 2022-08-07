f0cd82bae724bc2b27247036ceaf6e98500db97c
Umbra <icofundss@gmail.com>
2022-08-07 13:49:50 +0300

    refactor: настроить props в компоненте PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,14 +1,16 @@
 function PopupWithForm(props) {
-  //! в пропсах будет. id classOpen header _form SubName CloseBtn
+  const btnAvatarPosition = (props.name === 'formAddAvatar') ? 'popup__btn-close_type_position-avatar' : '';
+
   return (
-    <section id="popupEdit" className="popup popup_close popup_overlay" aria-label="всплывающее окно">
+    <section className={`popup popup_type_${props.name} popup_overlay 
+    ${props.isOpen ? 'popup_opened' : false} aria-label="всплывающее окно"`}>
       <div className="popup__container">
-        <h2 className="popup__edit-header">Редактировать профиль</h2>
-        <form className="form" id="formEdit" name="form-edit" noValidate>
-          {/* тут будет пропс children */}
-          <button className="popup__btn-save" type="submit">Сохранить</button>
+        <h2 className="popup__edit-header">{props.title}</h2>
+        <form className="form" id={`${props.name}`} name={`${props.name}`} noValidate>
+          <>{props.children}</>
+          <button className="popup__btn-save" type="submit">{props.btnName}</button>
         </form>
-        <button className="popup__btn-close" type="button"></button>
+        <button className={`popup__btn-close ${btnAvatarPosition}`} type="button" onClick={props.onClose}></button>
       </div>
     </section>
   )
