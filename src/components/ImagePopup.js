88258f2b5b92864259f0c9c5bc20554c5cb8ab18
Umbra <icofundss@gmail.com>
2022-08-07 14:00:55 +0300

    fix: исправить заголовок карточки. Изменение gitignore

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -9,7 +9,7 @@ function ImagePopup(props) {
         <button className="popup__btn-close popup__btn-close_type_position"
           onClick={props.onClose} type="button"></button>
         <img className="popup__image" src={props.card.link} alt={props.card.name} />
-        <figcaption className="popup__image-name"></figcaption>
+        <figcaption className="popup__image-name">{props.card.name}</figcaption>
       </figure>
     </section>
   )
