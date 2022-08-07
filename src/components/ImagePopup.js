c8614144bd07e7ea2ba53e6832aa9d35855dcc97
Umbra <icofundss@gmail.com>
2022-08-07 13:43:53 +0300

    feat: настроить props для компонента ImagePopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,9 +1,14 @@
-function ImagePopup() {
+import React from 'react';
+
+function ImagePopup(props) {
+
   return (
-    <section id="popupCardImg" className="popup popup_close popup_overlay" aria-label="всплывающее окно с картинкой">
+    <section id="popupCardImg" className={`popup popup_close popup_overlay 
+    ${props.card ? 'popup_opened' : false} aria-label="всплывающее окно с картинкой"`}>
       <figure className="popup__container-image">
-        <button className="popup__btn-close popup__btn-close_type_position" type="button"></button>
-        <img className="popup__image" src="#" alt="#" />
+        <button className="popup__btn-close popup__btn-close_type_position"
+          onClick={props.onClose} type="button"></button>
+        <img className="popup__image" src={props.card.link} alt={props.card.name} />
         <figcaption className="popup__image-name"></figcaption>
       </figure>
     </section>
