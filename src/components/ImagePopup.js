76ade71e7475aeaba4adb87fa4e1b5477b4e35cd
Umbra <icofundss@gmail.com>
2022-08-09 22:22:11 +0300

    fix: исправить фон попапа

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ImagePopup(props) {
 
   return (
-    <section id="popupCardImg" className={`popup popup_close popup_overlay 
+    <section id="popupCardImg" className={`popup popup_close popup_overlay popup_overlay_dark 
     ${props.card.isOpen ? 'popup_opened' : false} aria-label="всплывающее окно с картинкой"`}>
       <figure className="popup__container-image">
         <button className="popup__btn-close popup__btn-close_type_position"
