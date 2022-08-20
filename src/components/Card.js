12341f9e0f9b846797e2bea232c00627499f5dd9
Umbra <icofundss@gmail.com>
2022-08-20 23:50:35 +0300

    feat: добавить управление лайками

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,13 @@ function Card(props) {
   const currentUser = React.useContext(CurrentUserContext);
   // проверка id карточки с id моего пользователя.
   const isOwn = props.card.owner._id === currentUser._id;
-
-  const cardDeleteButtonClassName = (
-    `card__delete-button ${isOwn ? 'card__delete-button_visible' : 'card__delete-button_hidden'}`
+  const buttonTrashHidden = (
+    `gallery__btn-trash ${isOwn ? '' : 'gallery__btn-trash_hidden'}`
   );
+  console.log(props.onCardLike);
 
-
-  //const isLiked = props.card.likes.some(elementCard => elementCard._id === currentUser._id);
-  const cardLikeButtonClassName = `...`;
+  const isLiked = props.card.likes.some(elementCard => elementCard._id === currentUser._id);
+  const cardLikeButton = !isLiked ? '' : `gallery__btn-favorites_active`;
 
   //функция с данными карточки для работы попапа ImagePopup
   function handleClick() {
@@ -23,11 +22,11 @@ function Card(props) {
   return (
     <article className="gallery__card" >
       <img className="gallery__card-img" src={props.card.link} alt={props.card.name} onClick={handleClick} />
-      <button className="gallery__btn-trash" />
+      <button className={buttonTrashHidden} />
       <div className="gallery__card-header">
         <h3 className="gallery__card-name">{props.card.name}</h3>
         <div className="gallery__favorites-header">
-          <button className="gallery__btn-favorites" onClick={props.onCardLike} type="button" />
+          <button className={`gallery__btn-favorites ${cardLikeButton}`} onClick={props.onCardLike} type="button" />
           <div className="gallery__counter-favorites">{props.card.likes.length}</div>
         </div>
       </div>
