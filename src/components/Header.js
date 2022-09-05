b8562cffb613427ba92ac1335c31515c0064f9dd
Umbra <icofundss@gmail.com>
2022-09-05 23:47:33 +0300

    feat: изменить компонент Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-function Header() {
+import { Link } from "react-router-dom";
+
+function Header(props) {
   return (
     <header className="header">
-      <div className="header__logo"></div>
+      <div className="header__logo" />
+      <p className="header__email">xxx@email.com</p>
+      <Link className="header__authorization" to="/sign-in">Войти</Link>
     </header>
   )
 }
