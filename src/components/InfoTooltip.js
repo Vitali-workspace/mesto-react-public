d160036de3cc06642df83319260fa41bb135b72e
Umbra <icofundss@gmail.com>
2022-09-08 23:40:47 +0300

    feat: настроить props для InfoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,16 +1,20 @@
 
 function InfoTooltip(props) {
 
-  // {props.title}
-  // popup__container_type_tooltip
-  // {props.statusAuth ? "Вы успешно зарегистрировались!" : "Что-то пошло не так! Попробуйте ещё раз."}
+  const titleStatus = (props.isOpen.status === 'success')
+    ? 'Вы успешно зарегистрировались!'
+    : 'Что-то пошло не так! Попробуйте ещё раз.'
+  const iconStatus = (props.isOpen.status === 'success')
+    ? 'popup__icon-status'
+    : 'popup__icon-status-fail'
+
   return (
     <section
-      className={`popup popup_type_${props.name} popup_overlay 
-    ${props.isOpen ? 'popup_opened' : false} aria-label="всплывающее окно"`}>
+      className={`popup popup_overlay 
+    ${props.isOpen.open ? 'popup_opened' : false} aria-label="всплывающее окно"`}>
       <div className="popup__container popup__container_type_tooltip">
-        <div className="popup__icon-status"></div>
-        <h2 className="popup__edit-header">Вы успешно зарегистрировались!</h2>
+        <div className={iconStatus} />
+        <h2 className="popup__edit-header popup__edit-header_type_tooltip">{titleStatus}</h2>
         <button
           className={`popup__btn-close popup__btn-close_type_position-tooltip`}
           type="button"
